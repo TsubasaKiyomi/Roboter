44101b93d36236f1c0f2dea6aa3553ffba0fe7bc
TsubasaKiyomi <promisaring@icloud.com>
2020-04-16 06:51:16 +0900

    adjustment_addition

--- list-count.py
+++ list-count.py
@@ -1,5 +1,5 @@
 
-# import collections
+import collections
 
 string = [
     "nissan",
@@ -10,16 +10,20 @@ string = [
     "toyota",
     "toyota",
     "suzuki"]
-count = {'nissan': 0, 'toyota': 0, 'suzuki': 0, "daihatu": 0}
+print(collections.Counter(string))
+
+
+# count = {'nissan': 0, 'toyota': 0, 'suzuki': 0, "daihatu": 0}
 # impot collectionsをインポートしリストに対してcollections.Counter()を実行すると
 # ターミナルにCounter({})ができてキーとcount数が表示される。
-# c = collections.Counter(string)
-# print(c)
-for string in string:
-    count[string] += 1
-    # print(count)
-    # ",".join()で区別し文字列に
-    changes = ",".join(count)
-    # str.titleで単語毎に大文字へ
-    print(str.title(changes))
-    break
+# print(collections.Counter(string))
+
+
+# for string in string:
+#     count[string] += 1
+#     print(count)
+# ",".join()で区別し文字列に
+# changes = ",".join(count)
+# str.titleで単語毎に大文字へ
+# print(str.title(changes))
+# break
